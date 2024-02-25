fc66fb568f50f52c8b243c5d9f190051bbd2c13e
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-02-26 01:39:44 +0500

    header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { Container, Row, Button } from 'reactstrap'
-import Logo  from '../../assets/images/logo.png'
-
+import Logo from '../../assets/images/logo.png'
+import "./header.css"
 const nav__links = [
   {
     path: '/home',
@@ -13,7 +13,7 @@ const nav__links = [
     display: 'Tours'
   },
   {
-    path: '#',
+    path: '/about',
     display: 'About'
   },
 
@@ -24,14 +24,42 @@ function Header() {
     <header className="header">
       <Container>
         <Row>
-          <div className="nav__wrapper d-flex align-item-center justify-content-between">
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
             {/*  logo */}
             <div className="logo">
               <img src={Logo} alt="logo" />
             </div>
             {/* logo end */}
+
+            {/* menu start */}
             <div className="navigation">
-              <ul className='menu d-flex ali' ></ul>
+              <ul className='menu d-flex align-items-center gap-5 '>
+                {
+                  nav__links.map((item, index) => (
+                    <li className='nav__item' key={index}>
+                      <NavLink to={item.path} className={navClass=> navClass.isActive ? 'active__link' : ''
+                    }
+                    >
+                      {item.display}
+                    </NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+            {/* menu end */}
+            <div className="nav__right d-flex align-items-center gap-4 ">
+              <div className="nav__btns d-flex align-items-center gap-4 ">
+                <Button className='btn secondary__btn'>
+                  <Link to='/login' >Login</Link>
+                </Button>
+                <Button className='btn primary__btn'>
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+                <span class="mobile__menu">
+                  <i className='ri-menu-line'></i>
+                </span>
             </div>
           </div>
         </Row>
